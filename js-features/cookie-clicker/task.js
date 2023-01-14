a578942d8d349ed34f6ed5d632a3dfb7e7a713bb
artemro <artem.rozhdestvenskiy@gmail.com>
2023-01-14 21:24:57 +0300

    velocity fixed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -8,7 +8,7 @@ function clickHandler() {
     const cookieCounter = document.getElementById("clicker__counter");
     const currentDate = new Date();
     counter++;
-    cookieVelocity.textContent = (currentDate - firstDate) / (1000 * counter); // в секундах
+    cookieVelocity.textContent = (1000 * counter) / (currentDate - firstDate); // в секундах
     cookieCounter.textContent = counter;
     if (counter % 2 == 1) {
         cookieImage.width += 50;
