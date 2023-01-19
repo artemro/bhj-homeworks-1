33b9dada41a4dd8034183a9feb6690e514f342ee
artemro <artem.rozhdestvenskiy@gmail.com>
2023-01-19 23:45:56 +0300

    firstDate = currentDate

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,16 @@
 const cookieImage = document.getElementById("cookie");
 const cookieVelocity = document.getElementById("clicker__velocity"); 
-const firstDate = new Date();
+let firstDate = new Date();
 
 var counter = 0;
 
 function clickHandler() {
     const cookieCounter = document.getElementById("clicker__counter");
-    const currentDate = new Date();
+    let currentDate = new Date();
+    console.log(currentDate.getSeconds());
     counter++;
-    cookieVelocity.textContent = (1000 * counter) / (currentDate - firstDate); // в секундах
+    cookieVelocity.textContent = 1000 / (currentDate - firstDate); // в секундах
+    firstDate = currentDate;
     cookieCounter.textContent = counter;
     if (counter % 2 == 1) {
         cookieImage.width += 50;
