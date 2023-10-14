29a97693bb25aa7f7b893e4ada0a7cedbf7c83bf
Artem Rozhdestvenskiy <artem.rozhdestvenskiy@gmail.com>
2023-10-14 23:38:28 +0300

    extended countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,7 +1,10 @@
 const timerText = function(){
     const seconds = document.getElementById("timer");
-    let timer = 
+    const extendedTimer = document.getElementById("extended__timer")
+    let timer = new Date (seconds.textContent * 1000).toLocaleTimeString().slice(6);
     seconds.textContent -= 1;
+    timer -= 1;
+    extendedTimer.textContent = "00:00:" + timer;
     if (seconds.textContent == 0) {
         clearInterval(intervalId);
         alert("Вы победили в конкурсе!");
